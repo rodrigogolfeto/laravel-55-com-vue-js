62047f25a8aa3400d4794425b18492f2c67ae3b2
Rodrigo Golfeto <rgolfeto@gmail.com>
2018-07-18 13:32:29 -0400

    Aula 13 - Componente TabelaLista - Títulos

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -3,70 +3,7 @@
 @section('content')
 <pagina tamanho="12">
     <painel titulo="Lista de Artigos">
-      <a href="#">Criar</a>
-
-      <table class="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Título</th>
-            <th>Descrição</th>
-            <th>Autor</th>
-            <th>Data</th>
-            <th>Ação</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Título..</td>
-            <td>Descrição..</td>
-            <td>Autor..</td>
-            <td>Data..</td>
-            <td>
-              <a href="#">Editar</a> | 
-              <a href="#">Deletar</a>
-            </td>
-          </tr>
-
-          <tr>
-            <td>1</td>
-            <td>Título..</td>
-            <td>Descrição..</td>
-            <td>Autor..</td>
-            <td>Data..</td>
-            <td>
-              <a href="#">Editar</a> | 
-              <a href="#">Deletar</a>
-            </td>
-          </tr>
-
-          <tr>
-            <td>1</td>
-            <td>Título..</td>
-            <td>Descrição..</td>
-            <td>Autor..</td>
-            <td>Data..</td>
-            <td>
-              <a href="#">Editar</a> | 
-              <a href="#">Deletar</a>
-            </td>
-          </tr>
-
-          <tr>
-            <td>1</td>
-            <td>Título..</td>
-            <td>Descrição..</td>
-            <td>Autor..</td>
-            <td>Data..</td>
-            <td>
-              <a href="#">Editar</a> | 
-              <a href="#">Deletar</a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      
+      <tabela-lista v-bind:titulos="['teste','outros']"></tabela-lista>
     </painel>
 </pagina>
 @endsection
